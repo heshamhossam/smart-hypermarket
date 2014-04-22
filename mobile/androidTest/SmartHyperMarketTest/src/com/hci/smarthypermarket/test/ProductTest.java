5afdbf357026cff9930603430f69bc81171b7643
Neno0o <mike_memo2007@yahoo.com>
2014-04-22 16:54:49 +0200

    Android Testing

---
+++ mobile/androidTest/SmartHyperMarketTest/src/com/hci/smarthypermarket/test/ProductTest.java
@@ -0,0 +1,54 @@
+package com.hci.smarthypermarket.test;
+
+import com.hci.smarthypermarket.Product;
+
+import android.test.AndroidTestCase;
+
+public class ProductTest extends AndroidTestCase {
+	
+	//Test Product Constructor and its objects
+	public void testConstructor()
+	{
+		Product product = new Product("1", "ahmed", "12345", 10, "1");
+		assertNotNull(product);
+	}
+	
+	//Test Product Name
+	public void testName()
+	{
+		Product product = new Product();
+		product.setName("ahmed");
+		
+		String expected = "ahmed";
+		String actual = product.getName();
+		
+		assertEquals(expected, actual);
+	}
+	
+	//Test Product Barcode
+	public void testBarcode()
+	{
+		Product product = new Product();
+		product.setBarcode("12345");
+		
+		String expected = "12345";
+		String actual = product.getBarcode();
+		
+		assertEquals(expected, actual);
+		
+	}
+	
+	//Test Total Price of Purchased Produtcs
+	public void testTotalPrice()
+	{
+		Product product = new Product();
+		product.setPrice(10);
+		product.setPurchasedQuantity(2);
+		
+		float expected = 20;
+		float actual = product.getTotalPrice();
+		
+		assertEquals(expected, actual);
+	}
+
+}
