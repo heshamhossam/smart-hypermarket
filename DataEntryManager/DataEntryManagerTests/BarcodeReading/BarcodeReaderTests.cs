103953da42ab62634b45f6101f5dd5103af7d76b
theyaserfox <yaser.omar.abdelaziz@gmail.com>
2014-04-22 22:13:46 +0200

    Resolve some issue

---
+++ DataEntryManager/DataEntryManagerTests/BarcodeReading/BarcodeReaderTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BarcodeReading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace BarcodeReading.Tests
+{
+    [TestClass()]
+    public class BarcodeReaderTests
+    {
+        [TestMethod()]
+        public void readBarcodesTest()
+        {
+            
+            //BarcodeReader target = new BarcodeReader(;
+        }
+
+        [TestMethod()]
+        public void openCameraTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void BarcodeReaderTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ DataEntryManager/DataEntryManagerTests/ShowProductsTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Net;
+using System.IO;
+using System.Collections.Specialized;
+using DataEntryManager;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataEntryManager.Tests
+{
+    [TestClass()]
+    public class ShowProductsTests
+    {
+        [TestMethod()]
+        public void ShowProductsTest()
+        {
+            //ShowProducts target = new ShowProducts();
+            
+            //string expected = string.Empty;
+            //string actual;
+            //actual = target.InitializeComponent();
+            //Assert.AreEqual(expected, actual);
+            //Assert.Inconclusive("Verify the correctness of this test method.");
+            //Assert.Fail();
+        }
+    }
+}
