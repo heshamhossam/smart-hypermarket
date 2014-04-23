8cde415a0d192931cda10766557295f005174258
theyaserfox <yaser.omar.abdelaziz@gmail.com>
2014-04-23 18:26:07 +0200

    Sync minor changes

--- DataEntryManager/DataEntryManager/BarcodeReading/BarcodeReader.cs
+++ DataEntryManager/DataEntryManager/BarcodeReading/BarcodeReader.cs
@@ -111,7 +111,7 @@ namespace BarcodeReading
             webCamTimer.Interval = new TimeSpan(0, 0, 0, 0, 200);   //Assign interval of 200 milliseconds
             webCamTimer.Start();                                    //Start the timer
             //Fire OnBarcodeDetectedDelegate
-            onBarcodeDetected();         
+            onBarcodeDetected();
         }
         ~BarcodeReader() {
             _player.Dispose();
