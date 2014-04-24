dc4edcf3da7604c9b90cf7402bd1a69b52f80bb3
heshamhossam <heshamhossam57@gmail.com>
2014-04-24 06:08:21 +0200

    Splited the ServiceController into smaller ones

--- server/app/routes.php
+++ server/app/routes.php
@@ -8,35 +8,53 @@ Route::get('/', function()
 
 Route::get("/products/retrieve", array(
     "as" => "products-retrieve",
-    "uses" => "ServiceController@retrieveProduct"
+    "uses" => "ProductController@retrieve"
     
 ));
 
 Route::get("/products/create", array(
     "as" => "products-create",
-    "uses" => "ServiceController@getForm"
+    "uses" => "ProductController@formCreate"
     
 ));
 
 Route::post("/products/create", array(
     "as" => "products-create-post",
-    "uses" => "ServiceController@createProduct"
+    "uses" => "ProductController@create"
+    
+));
+
+Route::get("/products/delete", array(
+    "as" => "products-delete",
+    "uses" => "ProductController@delete"
+    
+));
+
+Route::post("/products/delete", array(
+    "as" => "products-delete-post",
+    "uses" => "ProductController@delete"
+    
+));
+
+Route::get("/products/edit", array(
+    "as" => "products-edit",
+    "uses" => "ProductController@edit"
     
 ));
 
 Route::post("/products/edit", array(
     "as" => "products-edit-post",
-    "uses" => "ServiceController@editProduct"
+    "uses" => "ProductController@edit"
     
 ));
 
 Route::get("/categories/retrieve", array(
     "as" => "categories-retrieve",
-    "uses" => "ServiceController@retrieveCategories"
+    "uses" => "MarketController@retrieveCategories"
 ));
 
 Route::get("/markets/retrieve", array(
     "as" => "markets-retrieve",
-    "uses" => "ServiceController@retrieveMarket"
+    "uses" => "MarketController@retrieve"
 ));
 
