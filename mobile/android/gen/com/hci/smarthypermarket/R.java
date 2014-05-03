5cc97d67d546e12bea2f74026d9b6ca6f05a3ea0
heshamhossam <heshamhossam57@gmail.com>
2014-05-04 00:51:03 +0200

    Added the order activity to be implemented

--- mobile/android/gen/com/hci/smarthypermarket/R.java
+++ mobile/android/gen/com/hci/smarthypermarket/R.java
@@ -65,8 +65,9 @@ public final class R {
         public static final int acticity_launcher=0x7f030000;
         public static final int activity_dash_board=0x7f030001;
         public static final int activity_main=0x7f030002;
-        public static final int activity_product=0x7f030003;
-        public static final int item_cart=0x7f030004;
+        public static final int activity_order=0x7f030003;
+        public static final int activity_product=0x7f030004;
+        public static final int item_cart=0x7f030005;
     }
     public static final class menu {
         public static final int dash_board=0x7f070000;
@@ -81,9 +82,9 @@ public final class R {
         public static final int add=0x7f050008;
         public static final int app_name=0x7f050000;
         public static final int back=0x7f050009;
-        public static final int brand=0x7f050012;
+        public static final int brand=0x7f050013;
         public static final int camera=0x7f050005;
-        public static final int description=0x7f050010;
+        public static final int description=0x7f050011;
         public static final int fireorder=0x7f05000d;
         public static final int hello_world=0x7f050002;
         public static final int home=0x7f050006;
@@ -91,16 +92,17 @@ public final class R {
         public static final int overflow=0x7f050004;
         /**  Product details 
          */
-        public static final int price=0x7f05000f;
+        public static final int price=0x7f050010;
         public static final int title_activity_dash_board=0x7f05000e;
+        public static final int title_activity_order=0x7f05000f;
         public static final int title_activity_product=0x7f050003;
-        public static final int title_browse=0x7f050015;
-        public static final int title_cart=0x7f050014;
-        public static final int title_orders=0x7f050016;
+        public static final int title_browse=0x7f050016;
+        public static final int title_cart=0x7f050015;
+        public static final int title_orders=0x7f050017;
         /**  DashBoard 
          */
-        public static final int title_scan=0x7f050013;
-        public static final int weight=0x7f050011;
+        public static final int title_scan=0x7f050014;
+        public static final int weight=0x7f050012;
     }
     public static final class style {
         /** 
---
+++ mobile/android/src/com/hci/smarthypermarket/OrderActivity.java
@@ -0,0 +1,25 @@
+package com.hci.smarthypermarket;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class OrderActivity extends Activity {
+
+	public OrderActivity() {
+		// TODO Auto-generated constructor stub
+	}
+	
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_order);
+		
+	}
+	
+	public void showOrder(Order order)
+	{
+		//implement it gemmy
+	}
+
+}
