52be27f01c66bf6586bc4fc62e1227af3fea031a
theyaserfox <yaser.omar.abdelaziz@gmail.com>
2014-05-08 23:58:52 +0200

    Add Market and Order tests

---
+++ Desktop/ModelsLibTests/MarketTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataEntryManager;
+using NUnit.Framework;
+using System.Runtime.Serialization;
+namespace DataEntryManager.Tests
+{
+    [TestFixture()]
+    public class MarketTests
+    {
+        [Test()]
+        public void getInstanceTest()
+        {
+            Market testMarket = Market.getInstance();
+            Assert.IsInstanceOf(typeof(Market), testMarket);
+        }
+
+        [Test()]
+        public void refreshOrdersTest()
+        {
+            Market testMarket = new Market(true);
+            Assert.AreEqual(0, testMarket.Orders.Count);
+            testMarket.refreshOrders();
+            //Assert.AreNotEqual(0, testMarket.Orders.Count);
+        }
+    }
+}
---
+++ Desktop/ModelsLibTests/OrderTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StorageManager.Models;
+using DataEntryManager;
+using NUnit.Framework;
+namespace StorageManager.Models.Tests
+{
+    [TestFixture()]
+    public class OrderTests
+    {
+        [Test()]
+        public void LoadOrdersTest()
+        {
+            //Arrange
+            List<Order> ret = Order.LoadOrders(Market.getInstance(), Order.ALL, "");
+            //Assert
+            Assert.AreEqual("0", ret[0].Id);
+        }
+
+        [Test()]
+        public void updateTest()
+        {
+            Order order = new Order();
+            order.update("");
+        }
+    }
+}
