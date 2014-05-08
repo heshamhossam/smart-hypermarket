71b57b36ef0032b83caee8a585b9e78918a077db
heshamhossam <heshamhossam57@gmail.com>
2014-05-08 22:38:15 +0200

    Added the browse activity

---
+++ mobile/android/src/com/hci/smarthypermarket/BrowseActivity.java
@@ -0,0 +1,18 @@
+package com.hci.smarthypermarket;
+
+import java.util.List;
+
+import android.app.Activity;
+
+public class BrowseActivity extends Activity {
+
+	public BrowseActivity() {
+		
+	}
+	
+	public void showProducts(List<Category> categories)
+	{
+		
+	}
+
+}
