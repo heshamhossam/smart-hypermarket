285a6a78b8d5a5c942e630f9bd1abc25d5d5d331
Kit <eslam_fcih@hotmail.com>
2014-05-10 07:32:46 -0600

    add new service for adding review to products

--- mobile/android/src/com/hci/smarthypermarket/models/Review.java
+++ mobile/android/src/com/hci/smarthypermarket/models/Review.java
@@ -1,5 +1,60 @@
 package com.hci.smarthypermarket.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.os.AsyncTask;
+
+
+
+abstract class SendReviewTask extends AsyncTask<Review, Integer, Review>
+{
+	
+	/////////////////Reviews Tags////////////////
+	final String Tag_ReviewBody="body";
+	final String Tag_ProductId="product_id";
+	final String Tag_UserId="user_id";
+	final String Tag_Updatedat="updated_at";
+	final String Tag_Createdat="created_at";
+	final String Tag_ReviewID="id";
+	final String Tag_UserMobile="mobile";
+	//////////////////////////////////////////
+	private static final String url_create_review=Model.linkServiceRoot + "/reviews/create";
+	JSONParser jsonParser = new JSONParser();
+	
+	
+	@Override
+	protected Review doInBackground(Review... params) {
+		// TODO Auto-generated method stub
+		List<NameValuePair> CParms = new ArrayList<NameValuePair>();
+		
+		CParms.add(new BasicNameValuePair(Tag_ProductId, params[0].product_id));
+		CParms.add(new BasicNameValuePair(Tag_ReviewBody, params[0].body));
+		CParms.add(new BasicNameValuePair(Tag_UserMobile, params[0].reviewer.getMobile()));
+		JSONObject jsonObject = jsonParser.makeHttpRequest(url_create_review, "GET", CParms);
+		try {
+			String Createdat=jsonObject.getString(Tag_Createdat);
+			String  Updatedat=jsonObject.getString(Tag_Updatedat);
+			String ReviewId=jsonObject.getString(Tag_ReviewID);
+			params[0].createdAt=Createdat;
+			params[0].updatedAt=Updatedat;
+			params[0].id=ReviewId;
+		} catch (JSONException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		return params[0];
+	}
+	  
+	
+}
+
+
 public class Review extends Model {
 	
 	protected Shopper reviewer;
@@ -7,6 +62,7 @@ public class Review extends Model {
 	protected String body;
 	protected String createdAt;
 	protected String updatedAt;
+	protected String product_id;
 	
 	
 	public Review(Shopper reviewer, String id, String body, String createdAt, String updatedAt) {
@@ -16,6 +72,7 @@ public class Review extends Model {
 		this.body = body;
 		this.createdAt = createdAt;
 		this.updatedAt = updatedAt;
+		
 	}
 
 	
@@ -32,16 +89,15 @@ public class Review extends Model {
 	
 	public void save()
 	{
-//		SendReviewTask sendReviewTask = new SendReviewTask() {
-//
-//			@Override
-//			protected void onPostExecute(Review result) {
-//				mirrior(result);
-//				modelHandler.OnModelSent();
-//			}
-//			
-//		};
-//		sendReviewTask.execute(this);
+		SendReviewTask sendReviewTask = new SendReviewTask() {
+		@Override
+	protected void onPostExecute(Review result) {
+		mirror(result);
+		modelHandler.OnModelSent();
+	}
+	
+};
+sendReviewTask.execute(this);
 		
 		
 		
