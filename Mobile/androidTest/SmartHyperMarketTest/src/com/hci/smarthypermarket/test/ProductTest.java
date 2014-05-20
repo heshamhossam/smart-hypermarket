da72c760e15c3436b8f96b0613de37765535b46a
Neno0o <mike_memo2007@yahoo.com>
2014-05-21 01:46:39 +0200

    MarketTest case

--- Mobile/androidTest/SmartHyperMarketTest/src/com/hci/smarthypermarket/test/ProductTest.java
+++ Mobile/androidTest/SmartHyperMarketTest/src/com/hci/smarthypermarket/test/ProductTest.java
@@ -2,14 +2,11 @@ package com.hci.smarthypermarket.test;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Semaphore;
 
 import com.hci.smarthypermarket.models.Product;
 import com.hci.smarthypermarket.models.Review;
 
-import android.net.sip.SipAudioCall.Listener;
 import android.test.AndroidTestCase;
-import junit.framework.TestCase;
 
 public class ProductTest extends AndroidTestCase {
 	
@@ -39,7 +36,6 @@ public class ProductTest extends AndroidTestCase {
 		list.add(new Review(null, "review2", 4));
 		list.add(new Review(null, "review3", 5));
 		list.add(new Review(null, "review4", 4));
-	//	float expectedrating = 14/4;
 		Product product = new Product("123", "pepsi", "1234", 10, "14", "disc", list);
 		int expected = product.getReviews().size();
 		Review review = new Review(null,"new review",20);
