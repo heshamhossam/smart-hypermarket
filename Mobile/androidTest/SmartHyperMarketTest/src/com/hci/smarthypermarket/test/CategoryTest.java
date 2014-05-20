39d2bb07de029a575e55bc29a20d7dbec66b9535
Neno0o <mike_memo2007@yahoo.com>
2014-05-20 23:44:01 +0200

    Category Test Case

--- Mobile/androidTest/SmartHyperMarketTest/src/com/hci/smarthypermarket/test/CategoryTest.java
+++ Mobile/androidTest/SmartHyperMarketTest/src/com/hci/smarthypermarket/test/CategoryTest.java
@@ -1,71 +1,54 @@
 package com.hci.smarthypermarket.test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.hci.smarthypermarket.models.Category;
+import com.hci.smarthypermarket.models.Offer;
+import com.hci.smarthypermarket.models.Product;
+
 import junit.framework.TestCase;
 
 public class CategoryTest extends TestCase {
 
 	public void testCategory() {
-		fail("Not yet implemented");
+		Category category = new Category();
+		assertNotNull(category);
 	}
 
 	public void testCategoryStringString() {
-		fail("Not yet implemented");
-	}
-
-	public void testGetId() {
-		fail("Not yet implemented");
-	}
-
-	public void testGetName() {
-		fail("Not yet implemented");
-	}
-
-	public void testGetCreatedAt() {
-		fail("Not yet implemented");
-	}
-
-	public void testGetUpdatedAt() {
-		fail("Not yet implemented");
-	}
-
-	public void testGetParentId() {
-		fail("Not yet implemented");
+		Category category = new Category("7", "Test");
+		assertNotNull(category);
 	}
 
 	public void testGetProducts() {
-		fail("Not yet implemented");
+		Category category = new Category();
+		List<Product> products = new ArrayList<Product>();
+		products.add(new Product("12", "Pepsi", "3607214116", (float) 24.5, "700", "this is a soda drink"));
+		products.add(new Product("23", "Headphone", "6920652801883", (float) 35.5, "700", "A Very Bad Headphone will damage your ears"));
+		category.setProducts(products);
+		
+		List<Product> products1 = category.getProducts();
+		
+		String actual = products1.get(0).getName();
+		String expected = "Pepsi";
+		
+		assertEquals(expected, actual);
 	}
 
-	public void testSetProducts() {
-		fail("Not yet implemented");
-	}
-
-	public void testAll() {
-		fail("Not yet implemented");
-	}
-
-	public void testGetAllCategories() {
-		fail("Not yet implemented");
-	}
 
 	public void testHasOffer() {
-		fail("Not yet implemented");
+		Category categoryBooks = new Category("5", "Books");
+		List<Product> products = new ArrayList<Product>();
+		products.add(new Product("12", "Pepsi", "3607214116", (float) 24.5, "700", "this is a soda drink"));
+		products.add(new Product("23", "Headphone", "6920652801883", (float) 35.5, "700", "A Very Bad Headphone will damage your ears"));
+		categoryBooks.setProducts(products);
+		List<Offer> offers = new ArrayList<Offer>();
+		offers.add(new Offer("offer test", "120", "Test offer teaser 1", "5-7-1993", "5-7-2014", categoryBooks.getProducts()));
+		
+		String actual = categoryBooks.hasOffer(offers).getName();
+		String expected = "offer test";
+		
+		assertEquals(expected, actual);
 	}
-
-	public void testGetBluetooth() {
-		fail("Not yet implemented");
-	}
-
-	public void testSetBluetooth() {
-		fail("Not yet implemented");
-	}
-
-	public void testGetOffer() {
-		fail("Not yet implemented");
-	}
-
-	public void testSetOffer() {
-		fail("Not yet implemented");
-	}
-
 }
