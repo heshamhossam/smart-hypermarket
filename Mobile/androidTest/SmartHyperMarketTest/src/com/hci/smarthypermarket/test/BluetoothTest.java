e659aef1bab61ca0aee5a532471816aaec545573
Neno0o <mike_memo2007@yahoo.com>
2014-05-20 22:53:06 +0200

    Android testing case

---
+++ Mobile/androidTest/SmartHyperMarketTest/src/com/hci/smarthypermarket/test/BluetoothTest.java
@@ -0,0 +1,51 @@
+package com.hci.smarthypermarket.test;
+
+import junit.framework.TestCase;
+
+public class BluetoothTest extends TestCase {
+
+	public void testBluetooth() {
+		fail("Not yet implemented");
+	}
+
+	public void testBluetoothStringStringInt() {
+		fail("Not yet implemented");
+	}
+
+	public void testBluetoothStringStringString() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	public void testSetName() {
+		fail("Not yet implemented");
+	}
+
+	public void testSetStrength() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetAddress() {
+		fail("Not yet implemented");
+	}
+
+	public void testSetAddress() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetStrength() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetId() {
+		fail("Not yet implemented");
+	}
+
+	public void testSetId() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ Mobile/androidTest/SmartHyperMarketTest/src/com/hci/smarthypermarket/test/CategoryTest.java
@@ -0,0 +1,71 @@
+package com.hci.smarthypermarket.test;
+
+import junit.framework.TestCase;
+
+public class CategoryTest extends TestCase {
+
+	public void testCategory() {
+		fail("Not yet implemented");
+	}
+
+	public void testCategoryStringString() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetId() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetCreatedAt() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetUpdatedAt() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetParentId() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetProducts() {
+		fail("Not yet implemented");
+	}
+
+	public void testSetProducts() {
+		fail("Not yet implemented");
+	}
+
+	public void testAll() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetAllCategories() {
+		fail("Not yet implemented");
+	}
+
+	public void testHasOffer() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetBluetooth() {
+		fail("Not yet implemented");
+	}
+
+	public void testSetBluetooth() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetOffer() {
+		fail("Not yet implemented");
+	}
+
+	public void testSetOffer() {
+		fail("Not yet implemented");
+	}
+
+}
